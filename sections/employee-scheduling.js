50410cad2cd2750f44ae929321f7c8cfa0aa2efe
Mohamed Elmenisy <m.elsayed@thechefz.co>
2025-04-19 10:03:32 +0200

    Update employee-scheduling.js

--- sections/employee-scheduling.js
+++ sections/employee-scheduling.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const addButton = document.getElementById('addEmployeeBtn');
-    if (addButton) {
-        addButton.addEventListener('click', function() {
-            alert("You clicked Add Employee!");
-        });
-    }
-});
+
